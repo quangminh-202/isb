72fbd9d5ad30e5d098319ae5aa673d2fd5f3dfa4
QuangMinh <quangminh01122002@gmail.com>
2024-03-16 22:01:02 +0400

    add exception

--- lab_1/task_1/simple_substitution_cipher.py
+++ lab_1/task_1/simple_substitution_cipher.py
@@ -16,18 +16,20 @@ def encrypt(text, key):
 
 # Генерация ключа шифрования на русском языке
 key = generate_key()
+try:
+    with open('original_text.txt', 'r', encoding='utf-8') as file:
+        original_text = file.read()
 
-with open('original_text.txt', 'r', encoding='utf-8') as file:
-    original_text = file.read()
+    # Шифрование текста
+    encrypted_text = encrypt(original_text, key)
 
-# Шифрование текста
-encrypted_text = encrypt(original_text, key)
+    # Сохранение зашифрованного текста
+    with open('encrypted_text.txt', 'w', encoding='utf-8') as file:
+        file.write(encrypted_text)
 
-# Сохранение зашифрованного текста
-with open('encrypted_text.txt', 'w', encoding='utf-8') as file:
-    file.write(encrypted_text)
-
-# Сохранение ключа шифрования
-with open('encryption_key.txt', 'w', encoding='utf-8') as file:
-    for k, v in key.items():
-        file.write(f"{k}:{v}\n")
+    # Сохранение ключа шифрования
+    with open('encryption_key.txt', 'w', encoding='utf-8') as file:
+        for k, v in key.items():
+            file.write(f"{k}:{v}\n")
+except Exception as e:
+    print("An error occurred:", str(e))
--- lab_1/task_2/decryption_code.py
+++ lab_1/task_2/decryption_code.py
@@ -2,23 +2,33 @@ import json
 def frequency_analysis(file_path):
     frequencies = {}
     total_chars = 0
-    with open(file_path, "r", encoding="utf-8") as file:
-        for line in file:
-            for char in line:
-                if char != "\n":
-                    total_chars += 1
-                    if char in frequencies:
-                        frequencies[char] += 1
-                    else:
-                        frequencies[char] = 1
-    relative_frequencies = {char: freq / total_chars for char, freq in frequencies.items()}
-    sorted_frequencies = sorted(relative_frequencies.items(), key=lambda x: x[1], reverse=True)
-    return sorted_frequencies
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            for line in file:
+                for char in line:
+                    if char != "\n":
+                        total_chars += 1
+                        if char in frequencies:
+                            frequencies[char] += 1
+                        else:
+                            frequencies[char] = 1
+    except FileNotFoundError:
+        print("File not found.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+    else:
+        relative_frequencies = {char: freq / total_chars for char, freq in frequencies.items()}
+        sorted_frequencies = sorted(relative_frequencies.items(), key=lambda x: x[1], reverse=True)
+        return sorted_frequencies
+
 
 def write_to_file(path: str, data: list) -> None:
-    with open(path, "w") as file:
-        for item in data:
-            file.write(f"{item[0]}: {item[1]}\n")
+    try:
+        with open(path, "w") as file:
+            for item in data:
+                file.write(f"{item[0]}: {item[1]}\n")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 def read_frequency_file(file_path: str) -> dict:
     frequency_dict = {}
@@ -30,24 +40,32 @@ def read_frequency_file(file_path: str) -> dict:
     return frequency_dict
 
 def key_decryption(frequency_text_path: str, standard_frequency_path: str, output_file: str) -> None:
-    frequency_text = read_frequency_file(frequency_text_path)
-    standard_frequency = read_frequency_file(standard_frequency_path)
-    key = {}
-    for char, freq_text in frequency_text.items():
-        closest_char = min(standard_frequency, key=lambda x: abs(float(freq_text) - float(standard_frequency[x])))
-        key[char] = closest_char
+    try:
+        frequency_text = read_frequency_file(frequency_text_path)
+        standard_frequency = read_frequency_file(standard_frequency_path)
+        key = {}
+        for char, freq_text in frequency_text.items():
+            closest_char = min(standard_frequency, key=lambda x: abs(float(freq_text) - float(standard_frequency[x])))
+            key[char] = closest_char
 
-    with open(output_file, 'w', encoding='utf-8') as json_file:
-        json.dump(key, json_file, ensure_ascii=False, indent=4)
+        with open(output_file, 'w', encoding='utf-8') as json_file:
+            json.dump(key, json_file, ensure_ascii=False, indent=4)
+    except Exception as e:
+        print(f"An error occurred {str(e)}")
 
 def decryption(input_file_path: str, output_file_path: str, key_path: str) -> None:
-    with open(key_path, "r", encoding="utf-8") as json_file:
-        decryption_key = json.load(json_file)
-    with open(input_file_path, 'r', encoding='utf-8') as input_file, \
-            open(output_file_path, 'w', encoding='utf-8') as output_file:
-        for line in input_file:
-            decrypted_line = ''.join(decryption_key.get(char, char) for char in line.strip())
-            output_file.write(decrypted_line + '\n')
+    try:
+        with open(key_path, "r", encoding="utf-8") as json_file:
+            decryption_key = json.load(json_file)
+        with open(input_file_path, 'r', encoding='utf-8') as input_file, \
+                open(output_file_path, 'w', encoding='utf-8') as output_file:
+            for line in input_file:
+                decrypted_line = ''.join(decryption_key.get(char, char) for char in line.strip())
+                output_file.write(decrypted_line + '\n')
+    except FileNotFoundError:
+        print("File not found.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 if __name__ == "__main__":
     #data = frequency_analysis(r"C:\Users\DELL\PycharmProjects\isb\lab_1\task_2\cod2.txt")
