8b98dfd7f9479951e2e6af2066bfca014aed5ea1
QuangMinh <quangminh01122002@gmail.com>
2024-03-30 00:11:29 +0400

    lab 2 done

--- lab_2/c_random_generator.cpp
+++ lab_2/c_random_generator.cpp
@@ -4,6 +4,14 @@
 
 #define SIZE 128
 
+/**
+ * @brief Generate a random binary sequence of the given size.
+ *
+ * This function generates a random binary sequence of the given size
+ * and prints it to the standard output.
+ *
+ * @param size The size of the binary sequence to generate.
+ */
 void random(int size) {
     srand(time(NULL));
     for (int i = 0; i < size; ++i) {
